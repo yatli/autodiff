7b31feb5193b803ba2bf6f553107dbce014ac337
Yatao Li <yatli@microsoft.com>
2020-05-10 17:31:47 +0800

    update log splitter

--- qnum/split_log.sh
+++ qnum/split_log.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
 logfile=$1
-expname=${logfile%-log.*}
+expname=${logfile%.log}
 echo "Splitting experiment log for $expname"
-echo "i,smp,loss,acc" > "$expname-train.csv"
-fgrep '[TRAIN]' "$logfile" | sed 's/\[TRAIN\] //g' | sed 's/\s*\w\w*\s*\=\s*//g' | sed  '/^epoch.*$/d' | awk '{print NR "," $s}' >> "$expname-train.csv"
+echo -e "num_updates\tepoch\tbatch_loss\tavg_loss\tacc" > "$expname-train.tsv"
+# sample line:
+#[TRAIN] epoch=   0 step=    40 batchloss=      2.30156 avgloss=      2.30449 acc=       0.1625 nupdates=         80
+# 1        2      3  4       5  6               7        8             9       10           11    12              13
+fgrep '[TRAIN]' "$logfile" | awk '{OFS="\t"} {print $13, $3, $7, $9, $11}' >> "$expname-train.tsv"
 fgrep '[DEBUG]' "$logfile" | sed 's/\[DEBUG\] //g' > "$expname-debug.txt"
-echo "epoch,loss,acc" > "$expname-test.csv"
-fgrep '[TEST]' "$logfile"  | sed 's/\[TEST\] //g' | paste -sd ',\n' | sed 's/[^0-9.,]//g' >> "$expname-test.csv"
+#sample line:
+# [TEST] epoch    0 avgloss=     0.153012 acc=       0.9532
+# 1       2       3  4              5     6              7
+echo -e "epoch\tloss\tacc" > "$expname-test.tsv"
+fgrep '[TEST]' "$logfile"  | awk '{OFS="\t"} {print $3, $5, $7}' >> "$expname-test.tsv"

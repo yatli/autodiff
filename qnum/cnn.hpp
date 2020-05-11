48b857d124ebaada89ffa85ec7fe6883a06cc697
Yatao Li <yatli@microsoft.com>
2020-05-12 00:20:55 +0800

    simplify cnn arch

--- qnum/cnn.hpp
+++ qnum/cnn.hpp
@@ -10,24 +10,22 @@ struct cnn_t : public nn_t<T> {
   int width;
   int height;
   int nclass;
-  std::vector<ndarray_t<T>> W1, W2, W3;
-  ndarray_t<T> b1, b2, b3;
+  std::vector<ndarray_t<T>> W1, W2;
+  ndarray_t<T> b1, b2;
   mat Wf1, Wf2;
 
   cnn_t(int c, int h, int w, int nclass)
     : nchannel(c), width(w), height(h), nclass(nclass),
-      b1(32, h, w, 0.05), b2(64, h/2, w/2, 0.05), b3(64, h/2, w/2, 0.05) {
-    W1.reserve(32);
-    W2.reserve(64);
-    W3.reserve(64);
-    for(int i=0;i<32;++i) { W1.emplace_back(c, 3, 3, 0.05); nn_t<T>::register_params(W1[i].v); }
-    for(int i=0;i<64;++i) { W2.emplace_back(32, 3, 3, 0.05); nn_t<T>::register_params(W2[i].v); }
-    for(int i=0;i<64;++i) { W3.emplace_back(64, 3, 3, 0.05); nn_t<T>::register_params(W3[i].v); }
+      b1(16, h, w, 0.05) , b2(32, h/2, w/2, 0.05)
+  {
+    W1.reserve(16);
+    W2.reserve(32);
+    for(int i=0;i<16;++i) { W1.emplace_back(c, 3, 3, 0.05); nn_t<T>::register_params(W1[i].v); }
+    for(int i=0;i<32;++i) { W2.emplace_back(16, 3, 3, 0.05); nn_t<T>::register_params(W2[i].v); }
     nn_t<T>::register_params(b1.v);
     nn_t<T>::register_params(b2.v);
-    nn_t<T>::register_params(b3.v);
-    Wf1 = mat::Random(512, 64 * h / 4 * w / 4 +1) * 0.05;
-    Wf2 = mat::Random(nclass, 512 + 1) * 0.05;
+    Wf1 = mat::Random(128, 32 * h / 4 * w / 4 +1) * 0.05;
+    Wf2 = mat::Random(nclass, 128 + 1) * 0.05;
     nn_t<T>::register_params(Wf1);
     nn_t<T>::register_params(Wf2);
   }
@@ -38,8 +36,7 @@ struct cnn_t : public nn_t<T> {
     auto x3 = maxpooling_2d(x2, 2, 2);
     dropout(x3.v, 0.25);
     auto x4 = conv2d_layer(x3, W2, b2, act_relu);
-    auto x5 = conv2d_layer(x4, W3, b3, act_relu);
-    auto x6 = maxpooling_2d(x5, 2, 2);
+    auto x6 = maxpooling_2d(x4, 2, 2);
     dropout(x6.v, 0.25);
     auto x7 = withb(x6.v);
     auto x8 = fc_layer(x7, Wf1, act_relu);
